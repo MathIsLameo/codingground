1d391e6137992def87c4855a100595c2718e50f1
MathIsLameo <geraghtr1@hawkmail.newpaltz.edu>
2017-02-05 16:40:10 -0500

    Created by Coding Ground Client

---
+++ New Project-20170205/main.c
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+#include <complex.h>
+
+int
+main(void)
+{
+    int size = 600;
+    int imax = 256;
+    int frames = 540;
+    double radius = 1.0;
+
+    for (int f = 0; f < frames; f++) {
+        double theta = f * 3.141592653589793 * 2.0 / frames;
+        printf("P6 %d %d 255\n", size, size);
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                complex double z =
+                    ((x / (size - 1.0) - 0.5) * radius * 2) +
+                    ((y / (size - 1.0) - 0.5) * radius * 2) * I;
+                complex double c =
+                    (-0.80 + 0.1 * sin(theta)) +
+                    (+0.00 + 0.1 * cos(theta)) * I;
+                int i = 0;
+                for (; i < imax && cabs(z) <= 2.0; i++)
+                    z = z * z + c;
+                int v = i * 255 / imax;
+                unsigned char pixel[] = {v, v, v};
+                fwrite(pixel, 1, 3, stdout);
+            }
+        }
+    }
+    return 0;
+}
